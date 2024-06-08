386c70b597373b849a423a8b0235dc5ddd0c16e5
jorgevels <jorgevelasquez006@hotmail.com>
2024-06-08 16:58:06 -0500

    Uploading project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import "./fonts.css";
 
 function App() {
+  const [dinosaur, setDinosaur] = useState(null);
+  const [error, setError] = useState("");
+
+  const fetchRandomDinosaur = async () => {
+    try {
+      const response = await axios.get(
+        `https://script.google.com/macros/s/AKfycbydyRvTTgmNfBjmTMWnS3NbeaTSmmR5eVtVTo03K36U46ukShua2sXkM_6IGByAQfI/exec`
+      );
+      console.log("API response:", response.data); // Depuración
+
+      // Verificar si la respuesta contiene datos de dinosaurio
+      if (
+        response.data &&
+        response.data.data &&
+        response.data.data.length > 0
+      ) {
+        const randomIndex = Math.floor(
+          Math.random() * response.data.data.length
+        );
+        setDinosaur(response.data.data[randomIndex]);
+        setError("");
+      } else {
+        setError("No dinosaur found in the API response.");
+        setDinosaur(null);
+      }
+    } catch (error) {
+      console.error("Error fetching dinosaur:", error); // Depuración
+      setError("Unable to fetch a random dinosaur. Please try again.");
+      setDinosaur(null);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dinosaurios</h1>
+        <button onClick={fetchRandomDinosaur}>Buscar Dinosarios</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {dinosaur && (
+          <div>
+            <h2>{dinosaur.name}</h2>
+            {dinosaur.image && (
+              <img
+                src={dinosaur.image}
+                alt={dinosaur.name}
+                style={{ maxWidth: "100%" }}
+              />
+            )}
+            <p>Especie: {dinosaur.Especie}</p>
+            <p>Tipo: {dinosaur.type}</p>
+          </div>
+        )}
       </header>
     </div>
   );
